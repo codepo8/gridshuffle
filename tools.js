da27f55ccd5d9d347ca0801a02caefdf88b58bf3
Christian Heilmann <chris.heilmann@gmail.com>
2025-11-19 10:17:14 +0100

    tools

--- tools.js
+++ tools.js
@@ -2,18 +2,18 @@
 
 
 const CSSprop = {
-    get: (elm, name) => getComputedStyle(elm).getPropertyValue('--' + name),
-    set: (elm, name, value) => elm.style.setProperty('--' + name, value)
+    get: (elm, name) => 
+        getComputedStyle(elm).getPropertyValue('--' + name),
+    set: (elm, name, value) => 
+        elm.style.setProperty('--' + name, value)
 };
 
 
 const rotatearray = (array, direction = 1) => {
-    return direction > 0 ? 
+     direction > 0 ? 
         array.push(array.shift()) : 
         array.unshift(array.pop());
 };
 // rotatearray([1,2,3])  => [2,3,1]
 // rotatearray([1,2,3], 1)  => [2,3,1]
 // rotatearray([1,2,3], -1) => [3,1,2]
-
-export { CSSprop, rotatearray };
